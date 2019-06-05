a4880324169d89a328e1a547555b7087b3db22ba
Sergey Bronzza <repinetskiy@gmail.com>
2019-06-05 18:27:39 +0300

    Initial commit

---
+++ src/main/java/MateAcad/BasicShop/BasicShopApplication.java
@@ -0,0 +1,13 @@
+package MateAcad.BasicShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicShopApplication.class, args);
+	}
+
+}
